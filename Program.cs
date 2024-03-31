800d4ce4ab96966ae419fe1d7a09bf36cfd43bce
Yaroslave1 <shutov200256@gmail.com>
2024-03-31 10:04:13 +0300

    Простой калькулятор

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+double a;
+double b;
+double result;
+char oper;
+Console.WriteLine("Введите первое число");
+a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите знак оператора");
+oper = Convert.ToChar(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+b = Convert.ToDouble(Console.ReadLine());
+
+if (oper == '+')
+{
+    result = a + b;
+    Console.WriteLine("Сумма " + a +" и "+ b + " равна " + result);
+}
+else if (oper == '-')
+{
+    result = a - b;
+    Console.WriteLine("Разность" + a + " и " + b + " равна " + result);
+}
+else if (oper == '*')
+{
+    result = a * b;
+    Console.WriteLine("Произведение " + a + " на " + b + " равно " + result);
+}
+else if (oper == '/')
+{
+    result = a / b;
+    Console.WriteLine("Деление " + a + " на " + b + " равна " + result);
+}
